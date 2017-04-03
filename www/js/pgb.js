0676ff32371f34ec7325b516877cfab022912abf
Karol <karol.cichon17@gmail.com>
2017-04-04 01:24:42 +0200

    internet connection

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -5,6 +5,15 @@ function init() {
 function onDeviceReady() {
 	navigator.notification.beep(2);
 }
+function startAcc() {
+    var options = { frequency: 3000 };  // Update every 3 seconds
+
+    watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+}
+function stopAcc() {
+    navigator.accelerometer.clearWatch(watchID);
+}
+
 
 function deviceInfo() {
 
@@ -29,4 +38,50 @@ function personaldata() {
 
 	navigator.notification.alert(info);
 	
+}
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN] = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI] = 'WiFi connection';
+    states[Connection.CELL_2G] = 'Cell 2G connection';
+    states[Connection.CELL_3G] = 'Cell 3G connection';
+    states[Connection.CELL_4G] = 'Cell 4G connection';
+    states[Connection.CELL] = 'Cell generic connection';
+    states[Connection.NONE] = 'No network connection';
+
+    navigator.notification.alert('Connection type: ' + states[networkState]);
+}
+function onOnline() {
+    navigator.notification.alert("Online");
+}
+function onOffline() {
+    navigator.notification.alert("Offline");
+}
+function onSuccess(acceleration) {
+
+    var p = document.querySelector('#accText');
+
+    p.innerHTML='Acceleration X: ' +
+        acceleration.x +
+        '<br/>' +
+        'Acceleration Y: ' +
+        acceleration.y +
+        '<br/>' +
+        'Acceleration Z: ' +
+        acceleration.z +
+        '<br/>' +
+        'Timestamp: ' +
+        acceleration.timestamp +
+        '<br/>';
+}
+
+function onError() {
+    navigator.notification.alert('onError!');
+}
+function showSplash() {
+    navigator.splashscreen.show();
 }
