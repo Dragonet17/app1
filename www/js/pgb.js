c98b94114355adced205dc66176b048cf2afd1d6
Karol <karol.cichon17@gmail.com>
2017-04-04 02:17:48 +0200

    sms and contacts

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,25 +1,20 @@
+﻿let watchID = null;
 function init() {
-	document.addEventListener("deviceready",onDeviceReady, false);
-}
+    document.addEventListener("deviceready", onDeviceReady, false);
+    document.addEventListener("online", onOnline, false);
+    document.addEventListener("offline", onOffline, false);
 
+}
 function onDeviceReady() {
-	//navigator.notification.beep(2);
-}
-function deviceInfo() {
-
-	info =  'Hi, I am your smartphone :-)' + '\n' +
-			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' + 
-			'Device Cordova : '  + device.cordova + '\n' + 
-			'Device Platform: ' + device.platform + '\n' + 
-			'Device UUID    : '     + device.uuid     + '\n' + 
-			'Device Model   : '    + device.model     + '\n' + 
-			'Device Version : '  + device.version  + '\n';
-
-	navigator.notification.alert(info);
-	
+    // navigator.notification.beep(1);
 }
 
+function personaldata() {
+    info = 'Name: Karol' + '\n' 
+          + 'Surname: Cichoń' + '\n' 
+          + 'Email: karol.cichon@gmail.com' ;
+        navigator.notification.alert(info);
+}
 function startAcc() {
     var options = { frequency: 3000 };  // Update every 3 seconds
 
@@ -63,7 +58,7 @@ function onSuccess(acceleration) {
 
     var p = document.querySelector('#accText');
 
-    p.innerHTML='Acceleration X: ' +
+    p.innerHTML = 'Acceleration X: ' +
         acceleration.x +
         '<br/>' +
         'Acceleration Y: ' +
@@ -80,7 +75,55 @@ function onSuccess(acceleration) {
 function onError() {
     navigator.notification.alert('onError!');
 }
+// Splashscreen
 function showSplash() {
     navigator.splashscreen.show();
+    setTimeout(function () {
+        navigator.splashscreen.hide();
+    }, 2000);
+}
+//End of splashscreen
+// Contacts
+function Contacts(contacts) {
+    let list = "";
+    for (var i = 0; i < contacts.length; i++) {
+        list += "Nazwa: " + contacts[i].displayName + " - " + contacts[i].phoneNumber + "\n";
+    }
+    navigator.notification.alert(list)
+};
+
+function onErrorC(contactError) {
+    navigator.notification.alert('onError!');
+};
+function showContacts() {
+    var optionsC = new ContactFindOptions();
+    optionsC.filter = "";
+    optionsC.multiple = true;
+    var filter = ["displayName", "phoneNumbers"];
+    navigator.contacts.find(filter, Contacts, onErrorC, optionsC);
 }
 
+function sendMessage() {
+    var message = document.querySelector("#message")
+
+}
+var app = {
+    sendSms: function () {
+        var number = document.getElementById('numberTxt').value;
+        var message = document.getElementById('messageTxt').value;
+        
+
+        //CONFIGURATION
+        var options = {
+            replaceLineBreaks: false, // true to replace \n by a new line, false by default
+            android: {
+                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                //intent: '' // send SMS without open any other app
+            }
+        };
+
+        var success = function () { alert('Wiadomość została wysłana'); };
+        var error = function (e) { alert('Błąd: ' + e); };
+        sms.send(number, message, options, success, error);
+    }
+};
